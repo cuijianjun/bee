6c985f46f63c97889f3e56125846f5a5af0fda75
zhenglihuan <zhenglh@tangdou.com>
2019-04-09 11:28:43 +0800

    修改测试环境

--- src/lib/api/address.js
+++ src/lib/api/address.js
@@ -1,2 +1,3 @@
 // export const REQUEST_HOST = 'https://rainbow.pplu.vip/';//请求接口 (正式)
-export const REQUEST_HOST = 'https://rainbow-dev.pplu.vip/';//请求接口（测试）
+// export const REQUEST_HOST = 'https://rainbow-dev.pplu.vip/';//请求接口（测试）
+export const REQUEST_HOST = 'https://127.0.0.1:3002/';//请求接口（测试）
